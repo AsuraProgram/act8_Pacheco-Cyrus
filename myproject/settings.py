91e606d194f5f3005105c600b57c62f4edca115a
AsuraProgram <120566232+AsuraProgram@users.noreply.github.com>
2026-06-01 20:12:12 +0800

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -35,6 +35,23 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'myproject.urls'
 
+# --- FIXED: Added the required TEMPLATES configuration block ---
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
@@ -50,12 +67,21 @@ REST_FRAMEWORK = {
     )
 }
 
+# --- FIXED: Added required internationalization defaults ---
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# --- FIXED: Added required primary key field default rule ---
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
